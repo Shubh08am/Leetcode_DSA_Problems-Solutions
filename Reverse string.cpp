f9da405b89605e216052d59d842e42c495077c27
Shubham Yadav <shubh08am@gmail.com>
2021-12-21 18:56:02 +0530

    Update Reverse string.cpp

--- Reverse string.cpp
+++ Reverse string.cpp
@@ -25,3 +25,35 @@ int start_index = 0 ;
     }
 
 };
+
+Approach II :-
+    
+class Solution {
+
+    public:
+
+        void reverseString(vector<char>& s) {
+
+        int i = 0;
+
+        int j = s.size() -1;
+
+        for(int x = 0; x<s.size()/2; x++){
+
+            char temp = s[i];
+
+            s[i] = s[j];
+
+            s[j] = temp;
+
+            i++;
+
+            j--;
+
+    
+
+    }
+
+    }
+
+};
