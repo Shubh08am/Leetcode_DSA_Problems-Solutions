ef0d6e4b62cc8038c91f8ca6c2b465ed2258f4c8
Shubham Yadav <shubh08am@gmail.com>
2022-02-01 20:07:44 +0530

    Create Binary search.cpp

---
+++ Binary search.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+
+        int l = 0;
+        int r= nums.size()-1;
+        
+        while(l<=r)
+        {int mid = l + (r-l)/2 ;
+            if(nums[mid]==target)
+            {   return mid; }
+            
+             else if(nums[mid]<target)
+            { l=mid+1;}
+            
+            else if(nums[mid]>target)
+            {r=mid-1;}
+            
+            
+ // int mid = l + (r-l)/2 ;
+            
+            
+        }
+        return -1;
+        
+    }
+};
