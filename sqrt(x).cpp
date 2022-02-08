0f2443e8db6ee5c322df06a999d08896388abda9
Shubham Yadav <shubh08am@gmail.com>
2022-02-08 20:05:51 +0530

    Update sqrt(x).cpp

--- sqrt(x).cpp
+++ sqrt(x).cpp
@@ -1,35 +1,30 @@
+
 class Solution {
 public:
-    int mySqrt(int x) {
-        
-  
-//float a = pow(x, 0.5);
-        //1st method
-
-        int a = sqrt(x);
-     
-return a;
-    }
-};
-
-        // 2nd method using binary search-
- /*       
-         int left = 1 ;
-        int right = x;
-while(left<right)
-{ 
-        int mid = (left +right)/2;
-        int temp = x/mid;
+    int mySqrt(int n) {
+          
+          int left = 0 ;
+          int right = n;
+         long long int mid = left +  (right-left)/2;
        
-if (temp==mid)
+        long long int ans=-1;
+        
+while(left<=right)
+{  long long int temp = mid*mid;
+       
+if (temp==n)
     return mid;
 
-      else  if (mid > temp) 
-            right = mid - 1;
+      if ( temp > n) 
+      right = mid - 1;
         
-else //if (mid < temp )
-left = mid-1 ;
+else if (n > temp )
+{  ans = mid;
+left = mid+1 ; 
 }
-            return(left -1 );
+    
+  mid = left +  (right-left)/2;  
+}
+            return ans;
     }
-};*/
+};
