b009fb54b3ca254abe0dd00c18b7c89c29f0431e
Shubham Yadav <shubh08am@gmail.com>
2022-02-10 19:16:59 +0530

    Update Find Peak Element.cpp

--- Find Peak Element.cpp
+++ Find Peak Element.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        if(nums.size()==2){ 
+        // 1st l.s approach
+     /*   if(nums.size()==2){ 
               if(nums[0]>nums[1])
                 return 0;
             
@@ -22,5 +23,29 @@ public:
                 
         }}
         return nums.size()-1;
+        */
+        
+        // 2nd l.s approach 
+        /*
+          for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] > nums[i + 1])
+                return i;
+        }
+        return nums.size() - 1; 
+        */
+        //3rd b.s approach 
+        int s = 0; 
+        int e = nums.size()-1 ;
+        int mid = s + (e-s)/2;
+        while(s<e){
+            if(nums[mid]<nums[mid+1])
+            { s=mid+1;    
+            }
+            else if (nums[mid]>nums[mid+1])
+            { e = mid;}
+            
+             mid = s + (e-s)/2;
+        }
+        return s;
     }
 };
