878d0d76defab769178ce86e109fb24ebaffe206
Shubham Yadav <shubh08am@gmail.com>
2022-02-13 11:18:17 +0530

    Create Count Operations to Obtain Zero.cpp

---
+++ Count Operations to Obtain Zero.cpp
@@ -0,0 +1,37 @@
+class Solution {
+
+public:
+
+    int countOperations(int num1, int num2) {
+
+       int cnt = 0;
+
+        while ( num1 != 0 && num2 != 0 ) 
+
+        { 
+
+            if(num1 >=num2 ) {
+
+                num1 = num1 - num2 ; 
+
+            }
+
+            
+
+            else if (num2> num1 ) 
+
+            {num2= num2 - num1 ; 
+
+        }
+
+            cnt++;
+
+            
+
+        }
+
+        return cnt ;
+
+    }
+
+};
