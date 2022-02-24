4fe594da0f06f456bb9539c1e9e5b5b1d59a1e76
Shubham Yadav <shubh08am@gmail.com>
2022-02-24 21:06:31 +0530

    Create Smallest Index With Equal Value.cpp

---
+++ Smallest Index With Equal Value.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int smallestEqual(vector<int>& nums) {
+          
+        for (int i=0 ; i<nums.size() ; i++)
+        {
+          if( nums[i] == i%10 ) {
+            return i ; 
+              break ;}
+        }
+    
+       return -1;
+    }
+};
