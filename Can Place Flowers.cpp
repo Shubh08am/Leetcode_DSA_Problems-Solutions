682bb66b3fa1da63d3875d2ab9fe789aa57d8265
Shubham Yadav <shubh08am@gmail.com>
2022-03-17 23:01:03 +0530

    Create Can Place Flowers.cpp

---
+++ Can Place Flowers.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+     int cnt = 0 ; 
+        int c = 0 ;
+    int l =  flowerbed.size();
+
+        for(int i = 0; i<l ;  i++){
+            bool lo = false, ro = false;
+            if(!flowerbed[i]){
+                if(i == 0 || !flowerbed[i-1]){
+                    lo = true;
+                }
+                if(i==l-1 || !flowerbed[i+1]){
+                    ro = true;
+                }
+                if(lo && ro){
+                    flowerbed[i] = 1;
+                    cnt++;
+                }
+            }
+        }
+        return cnt>=n;
+    }
+};
