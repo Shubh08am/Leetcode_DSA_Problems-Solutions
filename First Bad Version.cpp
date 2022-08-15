7c88b664e676b219a35d29f7b86dd80f202d1329
Shubham Yadav <shubh08am@gmail.com>
2022-08-15 19:11:05 +0530

    Update First Bad Version.cpp

--- First Bad Version.cpp
+++ First Bad Version.cpp
@@ -5,29 +5,30 @@ class Solution {
 public:
     int firstBadVersion(int n) {
         
-        int left = 1;
-        int right = n;
-        
-        int ans = 0;
-        
-        while(left<=right)
-        {
-            int mid = left + ( right - left )/2;
+        int s =0;
+        int e=n;
+        int mid ;
+        int ans = -1;
+        while(s<=e)
+            {
+       mid = s+(e-s)/2;
+            
+          if(isBadVersion(mid)){
+              ans=mid;
+            e=mid-1;
+          }
+         
+        else {
+           s=mid+1; 
+            }
            
-            if(isBadVersion(mid))
-            {
-                ans = mid; 
-                 right= mid -1;
+            
+            
             }
-            else
-            {
-                left = mid+1;
-            }
-        }
-        
-        return ans;    
-        
-      
-        
+         
+
+
+
+return ans ;
     }
 };
