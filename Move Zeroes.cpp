addf9ddb5cdac4d15b1bcca9dfd9e07a6e983430
Shubham Yadav <shubh08am@gmail.com>
2022-08-29 21:18:55 +0530

    Update Move Zeroes.cpp

--- Move Zeroes.cpp
+++ Move Zeroes.cpp
@@ -1,35 +1,21 @@
 class Solution {
-
 public:
-
-    void moveZeroes(vector<int>& nums) {
-
-  vector<int>v ;
-
-        v=nums; 
-
-        int cnt = 0;
-
-        nums.clear();
-
-        for(int i=0 ; i<v.size();i++){ if(v[i] == 0) cnt++;
-
-                                     } 
-
-for(int i=0 ; i<v.size();i++){
-
-if(v[i]!=0) nums.push_back(v[i]) ;    
-
-}
-
-for(int i=1 ; i<=cnt ; i++){
-
-    
-
-    nums.push_back(0);}
-
+    void moveZeroes(vector<int>& nums) { 
+        vector<int>v(nums.size()); 
+  int e=nums.size();
+    v=nums ;
+      int o=0 ;  
+  nums.clear(); 
+      for(int i=0 ; i<e ; i++){
+    if(v[i]!=0){
+        nums.push_back(v[i]) ;}
+          else o++;
+          
+          }
+       while(o--){
+           nums.push_back(0);
+           }
         
-
+      
     }
-
 };
