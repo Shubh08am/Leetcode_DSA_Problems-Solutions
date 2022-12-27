d85c36df805c09c1100511d7125196cc3e1f64cf
Shubham Yadav <shubh08am@gmail.com>
2022-12-27 11:07:45 +0530

    Create 479. Largest Palindrome Product.cpp

---
+++ 479. Largest Palindrome Product.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int largestPalindrome(int n) {
+        // badest solution for badest question :)
+        vector<int>v(9);
+        v[1]=9;
+        v[2]=987;
+        v[3]=123;
+        v[4]=597;
+        v[5]=677;
+        v[6]=1218;
+        v[7]=877;
+        v[8]=475;
+
+        return v[n];
+    }
+};
