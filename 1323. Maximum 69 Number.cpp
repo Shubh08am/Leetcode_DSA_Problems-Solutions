1d8a891f4e07c03691fa09658f80ed357efa5625
Shubham Yadav <shubh08am@gmail.com>
2022-12-29 00:22:44 +0530

    Create 1323. Maximum 69 Number.cpp

---
+++ 1323. Maximum 69 Number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+string nums = to_string(num);
+for(int i=0;i<nums.size();i++){
+	if(nums[i]=='6'){
+    		nums[i]='9';
+            		break;
+                    	}
+                        }
+
+                        int res = stoi(nums);
+                        return res;        
+    }
+};
