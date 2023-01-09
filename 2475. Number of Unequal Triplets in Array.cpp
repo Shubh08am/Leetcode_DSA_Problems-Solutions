f291d903f8b73af24dcf76af2f9ec1f4f6bc9e36
Shubham Yadav <shubh08am@gmail.com>
2023-01-09 20:12:52 +0530

    Create 2475. Number of Unequal Triplets in Array.cpp

---
+++ 2475. Number of Unequal Triplets in Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int unequalTriplets(vector<int>& nums) {
+int n=nums.size();
+int cnt=0;
+for(int i=0;i<n-2;i++){
+	for(int j=i+1;j<n-1;j++){
+		for(int k=j+1;k<n;k++){
+if(nums[i]!=nums[j] && nums[k]!=nums[j] && nums[i]!=nums[k] ) cnt++;			
+		}}}
+	return cnt;        
+    }
+};
