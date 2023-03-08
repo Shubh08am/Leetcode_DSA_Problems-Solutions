bfc216e6ed9b7ee5534ffde952bad678d5b80574
Shubham Yadav <shubh08am@gmail.com>
2023-03-08 15:05:00 +0530

    Update 1091. Shortest Path in Binary Matrix.cpp

--- 1091. Shortest Path in Binary Matrix.cpp
+++ 1091. Shortest Path in Binary Matrix.cpp
@@ -10,7 +10,7 @@ public:
         // (2) if n==1 and only 0 present return 1 or if at top-left or bottom-right no 0 return -1
          if(grid[0][0]!=0 || grid[n-1][m-1]!=0) return -1;
         if(grid[0][0]==0 && n==1) return 1;
-        
+
          vector<vector<int>>vis(n,vector<int>(m,0)) ; 
          // mark source as visited 
          vis[source.first][source.second]=1;
@@ -28,17 +28,17 @@ public:
              int dis = it.first ; 
              int row = it.second.first , col = it.second.second; 
              // or return here 
-           // if(row==destination.first && col==destination.second) return dis;
+            if(row==destination.first && col==destination.second) return dis+1;
              //traverse all 8 direction 
              for(int i=0;i<8;i++){
                  int newRow = row + dx[i] ; 
                  int newCol = col + dy[i] ; 
       
       
-                 //check valid && 0 at grid  
-         if(newRow>=0 && newRow<n && newCol>=0 && newCol<m && grid[newRow][newCol]==0 && !vis[newRow][newCol]){
+                 //check valid && 0 at grid  && not visited
+         if(newRow>=0 && newRow<n && newCol>=0 && newCol<m && !grid[newRow][newCol] && !vis[newRow][newCol]){
                 //reach destination 
-      if(newRow==destination.first && newCol==destination.second) return dis + 2;
+    //  if(newRow==destination.first && newCol==destination.second) return dis + 2;
                 vis[newRow][newCol] =  1 ;
                 q.push({dis + 1 ,{newRow,newCol}});
             }
