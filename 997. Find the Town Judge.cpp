d45c259c7e23b0eb59a0bb5855064ca8cb3b1bb8
Shubham Yadav <shubh08am@gmail.com>
2023-03-18 18:12:26 +0530

    Create 997. Find the Town Judge.cpp

---
+++ 997. Find the Town Judge.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        
+        // method 1 using two array
+        // tc- O(n+n) = O(n)
+        
+        vector<int> trustedbypeople(n+1 , 0);
+        vector<int> trusting(n+1 , 0);
+        
+        for(auto& x : trust )
+        {
+            trustedbypeople[x[1]]++;
+            trusting[x[0]]++;
+            
+        }
+        
+        for ( int i =1 ; i<= n ; i++)
+        {
+            if ( trustedbypeople[i] == n-1 &&  trusting[i]==0)
+                return i;
+        }
+        return -1 ;
+    }
+};
