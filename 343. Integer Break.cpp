6a2e3a2b98cdbd9128a105797721430c1e6b63d8
Shubham Yadav <shubh08am@gmail.com>
2023-04-10 21:12:26 +0530

    Update 343. Integer Break.cpp

--- 343. Integer Break.cpp
+++ 343. Integer Break.cpp
@@ -1,28 +1,45 @@
 class Solution {
 public:
-    int integerBreak(int n) {
-        //8 - 18(3 3 2)
-        //9 - 27(3 3 3)
-        //15 - 243(3 3 3 3 3)  
-        //16 - 324 (3 3 3 3 4)
-        //17-  486 (3 3 3 3 3 2)
-        //18 - 486 (3 3 3 3 3 3)
-        int ans=1;
-        if(n==2) return 1; // 1(1 1) 
-        if(n==3) return 2 ;  // 2(2 1) 
-        if(n==4) return 4 ; // 4(2 2)
-        //use 3 till n >4 
-        while(1){
-               if(n<=4){ 
-                   ans*=n; 
-                   return ans;
-                   }
-                else{
-                    ans*=3;
-                    n-=3;
-                }
+    //Approach 1:- Recursion
+    int solve(int n , int k){
+         //base case:- when breaked into k part  
+         if(k==0){
+             return n;
+         }
+         //Now,break into k parts 
+         int ans=1;
+         for(int i=1;i<n;i++){
+             //breaked into 1 i.e k reduces by 1 
+             //and n decreases by i 
+             ans=max(ans,i*solve(n-i,k-1));
+         }
+        return ans;
+    }
+    //Approach 2:- Memoization
+        int solve2(int n , int k,vector<vector<int>>&dp){
+         //base case:- when breaked into k part  
+         if(k==0){
+             return n;
+         } 
+         if(dp[n][k]!=-1)  return dp[n][k];
 
+         //Now,break into k parts 
+         int ans=1;
+         for(int i=1;i<n;i++){
+             //breaked into 1 i.e k reduces by 1 
+             //and n decreases by i 
+             ans=max(ans,i*solve2(n-i,k-1,dp));
+         }
+        return dp[n][k]=ans;
+    }
+    int integerBreak(int n) {
+        //try all combination of breaking 
+        int res=1; //atleast 1 answer 
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        for(int i=1;i<n;i++){
+           // res=max(res,solve(n,i)); 
+            res=max(res,solve2(n,i,dp)); 
         }
-        return 1;
+        return res;
     }
 };
