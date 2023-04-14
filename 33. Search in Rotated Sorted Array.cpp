af520c58dd4c14255c13551b95d39a35f80973a2
Shubham Yadav <shubh08am@gmail.com>
2023-04-14 19:51:18 +0530

    Create 33. Search in Rotated Sorted Array.cpp

---
+++ 33. Search in Rotated Sorted Array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start=0,end=nums.size()-1;
+
+        while(start<=end){
+            int mid= start+(end-start)/2;
+
+            if(nums[mid]==target)  return mid;
+            else if(nums[start]<=nums[mid]){
+                if(target<=nums[mid] && target>=nums[start]) end=mid-1;
+                else start=mid+1;
+            }
+            
+            else{
+                if(target>=nums[mid] && target<=nums[end])   start=mid+1;
+                else  end=mid-1;
+            }
+            
+        }
+        return -1;
+    }
+};
