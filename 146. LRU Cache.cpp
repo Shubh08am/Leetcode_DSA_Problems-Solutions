cb3ab928975b5cd47f2a1d8297fc989e336fb5fe
Shubham Yadav <shubh08am@gmail.com>
2023-06-01 18:31:57 +0530

    Create 146. LRU Cache.cpp

---
+++ 146. LRU Cache.cpp
@@ -0,0 +1,95 @@
+class Node{
+public:
+    int key,val ; 
+    Node*next;
+    Node*prev;
+
+    Node(int _key , int _val){
+    key=_key;
+    val=_val;
+}
+};
+
+class LRUCache {
+public:
+    Node*head = new Node(-1,-1)  ; //DUMMY VALUE ASSIGNED
+    Node*tail = new Node(-1,-1) ; 
+    
+    int capacity;
+    unordered_map<int,Node*>m;
+
+    LRUCache(int capacity) {
+       this->capacity=capacity;
+       head->next=tail ; 
+       tail->prev=head; 
+    }
+  
+    //T.C :- O(1) [ONLY LINK CHANGED]
+   void addNode(Node* newnode){
+       Node*temp = head->next ; 
+       newnode ->next = temp ; 
+       newnode->prev = head ; 
+       head->next = newnode ; 
+       temp->prev = newnode ;
+   }
+
+   //T.C :- O(1) [ONLY LINK CHANGED]
+   void deleteNode(Node* delNode){
+       Node*delPrev = delNode->prev ; 
+       Node*delNext = delNode->next ; 
+       delPrev->next = delNext ; 
+       delNext->prev = delPrev ;
+   }
+
+
+    //T.C :- O(1) 
+    int get(int key) {
+       if(m.find(key)!=m.end()){
+           Node*curr  = m[key] ; 
+           int value = curr->val ; 
+
+            //1. DELETE FROM CACHE 
+             deleteNode(curr) ; 
+            
+            //2. INSERT IN CACHE RIGHT AFTER HEAD 
+            addNode(curr) ; 
+
+            //3. UPDATE CURRENT ADDRESS [ head->next ] OF KEY IN HASH-MAP 
+            m[key] = head->next ; 
+
+           return value;
+       }
+       return -1; //WHEN KEY NOT PRESENT 
+    }
+    
+    //T.C :- O(1) 
+    void put(int key, int value) {
+
+        //IF ALREADY EXIST IN CACHE :- DELETE AND INSERT AS MOST RECENTLY USED [WITH NEW ADDRESS]
+        if(m.find(key)!=m.end()){
+                Node* existingNode = m[key] ; 
+                m.erase(key) ; //erase from hash-map previous address of node 
+                deleteNode(existingNode) ;
+        }
+
+        //IF CACHE CAPACITY IS FULL DELETENODE 
+        if(m.size()==capacity){
+            //DELETE LEAST RECENTLY USED WHICH EXIST BEFORE TAIL I.E PREVIOUS NODE TO TAIL
+             m.erase(tail->prev->key) ; //erase from hash-map previous address of node 
+            deleteNode(tail->prev) ; 
+        }
+
+        //NOW INSERT IN CACHE RIGHT AFTER HEAD 
+        addNode(new Node(key,value)) ; 
+
+        // UPDATE CURRENT ADDRESS [ head->next ] OF KEY IN HASH-MAP 
+         m[key] = head->next ; 
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
