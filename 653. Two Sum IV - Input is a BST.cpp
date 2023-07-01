69b31733ad93d5f919c2af23a8126decc166a519
Shubham Yadav <shubh08am@gmail.com>
2023-07-01 16:38:03 +0530

    Update 653. Two Sum IV - Input is a BST.cpp

--- 653. Two Sum IV - Input is a BST.cpp
+++ 653. Two Sum IV - Input is a BST.cpp
@@ -9,17 +9,46 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class BSTIterator {
+public:
+    stack<TreeNode*>st;
+    bool flag = 0;
+    void pushAll(TreeNode*root){
+        while(root){
+            st.push(root);
+            if(!flag) root=root->left;
+            else root=root->right;
+        }
+    }
+    BSTIterator(TreeNode* root,bool flg) {
+        flag=flg;
+        pushAll(root);
+    }
+    
+    int next() {
+        TreeNode* tos = st.top();
+        st.pop();
+       if(!flag) pushAll(tos->right);
+       else pushAll(tos->left);
+        return tos->val;
+    }
+    
+    bool hasNext() {
+        return !st.empty();
+    }
+};
 class Solution {
 public:
-     unordered_map<int,int>freq;
     bool findTarget(TreeNode* root, int k) {
-        //brute force uses s.c -> o(n) 
-        if(root==NULL) return false;
-       if(freq.find(k-root->val) != freq.end()) return true;
-        freq[root->val]++;
-       if (findTarget(root->left,k)) return true;
-       else if( findTarget(root->right,k)) return true;
+        BSTIterator l(root,0);  //next
+        BSTIterator r(root,1); //before
+        int i = l.next() , j=r.next();
 
-       return false;
+        while(i<j){
+            if(i+j==k) return 1;
+            if(i+j<k) i=l.next();
+            if(i+j>k) j=r.next();
+        }
+    return 0;
     }
 };
