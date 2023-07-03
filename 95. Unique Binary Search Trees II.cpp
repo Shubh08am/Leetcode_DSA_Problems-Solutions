69dd2e6bfb7a06cadf6d7f6754416ce96980c87b
Shubham Yadav <shubh08am@gmail.com>
2023-07-03 15:41:38 +0530

    Update 95. Unique Binary Search Trees II.cpp

--- 95. Unique Binary Search Trees II.cpp
+++ 95. Unique Binary Search Trees II.cpp
@@ -11,36 +11,37 @@
  */
 class Solution {
 public:
-    vector<TreeNode*>solve(int s,int e){
+    vector<TreeNode*>solve(int s,int e,map<pair<int,int>,vector<TreeNode*>>&dp){
         vector<TreeNode*>bst; 
         if(s>e){
             bst.push_back(NULL);
             return bst;
-        }
+        } 
         if(s==e){
             bst.push_back(new TreeNode(s));
             return bst;
         }
+        if(dp.find({s,e})!=dp.end()) return dp[{s,e}];
          for(int j=s;j<=e;j++){ 
-            auto left = solve(s,j-1) ; 
-            auto right= solve(j+1,e) ;
+            auto left = solve(s,j-1,dp) ; 
+            auto right= solve(j+1,e,dp) ;
             
             //Now,exploring all ways 
             for(auto&l:left){
                 for(auto&r:right){
-                    TreeNode*root= new TreeNode(j) ; 
-                    root->left = l ; 
-                    root->right = r;
+                    // (root,root->left,root->right)
+                    TreeNode*root= new TreeNode(j,l,r) ;  
                     bst.push_back(root);
                 }
             }
         }
-    return bst;
+    return dp[{s,e}]=bst;
     }
     vector<TreeNode*> generateTrees(int n) {
     /*       i 
         i-1      n-i 
     */
-    return solve(1,n);
+    map<pair<int,int>,vector<TreeNode*>>dp;
+    return solve(1,n,dp);
     }
 };
