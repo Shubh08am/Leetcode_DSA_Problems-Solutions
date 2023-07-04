ec9efcdfac7e2bd0caa2af128a95af05c72d9eca
Shubham Yadav <shubh08am@gmail.com>
2023-07-04 20:06:48 +0530

    Update Single Number II.cpp

--- Single Number II.cpp
+++ Single Number II.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int>mp ;
-        for(auto i : nums){
-           mp[i]++;
-            } 
-        for(auto t : mp)
-            {
-            if(t.second==1){
-                return t.first ;
-                }
-            } 
-        return -1;
+        //bits-manipulation 
+        int num=0 , n = nums.size() ; 
+        for(int i=0;i<32;i++){
+            int setBit = 0 ;
+            for(int j=0;j<n;j++){
+                 if((nums[j]&(1<<i))) setBit++;
+            }
+           // cout<<setBit<<"\n";
+            //group of 3 i.e divisible by 3 
+            if(setBit%3!=0) num = num ^ (1<<i) ;
+        }
+    return num;
     }
 };
