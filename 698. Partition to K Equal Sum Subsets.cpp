fa8f6c2c1d52147d5bd413d8ba58c67e5773788c
Shubham Yadav <shubh08am@gmail.com>
2023-07-06 19:21:23 +0530

    Update 698. Partition to K Equal Sum Subsets.cpp

--- 698. Partition to K Equal Sum Subsets.cpp
+++ 698. Partition to K Equal Sum Subsets.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-    bool solve(int ind,int curr_sum ,int target, int k, vector<int>& nums,vector<int>&vis) {
-        int n = nums.size();
-        if(ind>=n) return 0; 
-        if (k == 1) return 1;
-         
+    bool solve(int ind,int curr_sum ,int target, int k,int mask, vector<int>& nums,vector<int>&dp) { 
+        if (k == 0) {
+            return 1;
+        }   
+        if(dp[mask]!=-1) return dp[mask];
+
         if (curr_sum == target){
-            return solve(0,0,target,k-1,nums,vis);
+            return solve(0,0,target,k-1,mask,nums,dp);
         }
-        
-        for (int i = ind; i < n; i++) {
-            if (vis[i]) continue;
-            if(curr_sum + nums[i] <= target){
-            vis[i] = 1;
-            if (solve(i+1, curr_sum + nums[i],target,k, nums, vis)) return 1;
-            //backtrack
-            vis[i] = 0;
+        for (int j = ind; j < nums.size(); j++) {
+            if (mask&(1<<j)) continue;
+            if(curr_sum + nums[j] <= target){
+          if (solve(j+1, curr_sum + nums[j],target,k,mask|(1<<j),nums, dp)) return dp[mask]=1;
             }
         }
-        return 0;
+        return dp[mask]= 0;
     }
-
+    
     bool canPartitionKSubsets(vector<int>& nums, int k) {
-        //backtracking approach
+         //backtracking approach
          //find total subsets with sum/k 
          int totalSum=0 , n = nums.size();
-         vector<int>vis(n,0);
+         vector<int>dp(1<<16,-1);
          for(auto&val:nums) totalSum+=val;
         int target = totalSum/k;
 
          if(totalSum%k!=0 || n<k) return 0;
-         return solve(0,0,target,k,nums,vis);  
+         return solve(0,0,target,k,0,nums,dp);  
 }
 };
