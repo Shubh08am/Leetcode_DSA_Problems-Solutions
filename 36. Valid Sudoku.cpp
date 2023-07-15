019eb85afd9baf2efcbf3a2177317dc5ca88173f
Shubham Yadav <shubh08am@gmail.com>
2023-07-15 17:03:10 +0530

    Update 36. Valid Sudoku.cpp

--- 36. Valid Sudoku.cpp
+++ 36. Valid Sudoku.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
-
     bool isValidSudoku(vector<vector<char>>& board) {
-    int row[9][9] = {0};
-    int col[9][9] = {0};
-    int sub[3][3][9] = {0};
-    
-    for(int i=0;i<9;i++){
-        for(int j=0;j<9;j++)  {
-            if(board[i][j]=='.') {
-            continue;
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                char score = board[i][j] ; 
+                if(score=='.') continue;
+                for(int k=0;k<9;k++){
+                    //row check 
+                    if(k!=j && board[i][k]==score) {
+                       // cout<<i<<" "<<j<<" "<<k<<"\n";
+                        return false;
+                    }
+                    //col check 
+                    if(k!=i && board[k][j]==score) {
+                       //  cout<<i<<" "<<j<<" "<<k<<"\n";
+                        return false;
+                    }
+                    //3*3 sub-matrix check 
+                    int nr = 3*(i/3)+k/3;
+                    int nc = 3*(j/3)+k%3; 
+                    if(nr!=i && nc!=j && board[nr][nc]==score) {
+                       // cout<<nr<<" "<<nc<<" "<<i<<" "<<j<<" "<<k<<"\n";
+                        return false;
+                    }
+                }
             }
-
-            int check = board[i][j]-'1';
-            if(col[check][j]++ || row[i][check]++ || sub[i/3][j/3][check]++){
-            return false;
-            }
-                
         }
-    }
-    
     return true;
     }
 };
