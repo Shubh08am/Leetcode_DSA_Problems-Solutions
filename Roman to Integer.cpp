f2d70167102e4897c2b9c7844a0d13ab1b1c61bb
Shubham Yadav <shubh08am@gmail.com>
2023-07-21 12:35:25 +0530

    Update Roman to Integer.cpp

--- Roman to Integer.cpp
+++ Roman to Integer.cpp
@@ -1,49 +1,12 @@
 class Solution {
 public:
-    int romanToInt(string s) {
-     //    string a = "IV";
-     //  string b = "IX";
-     // string c = "XL";
-     // string d = "XC";
-     // string e = "CD";
-     // string f = "CM";
-int ans=0;
-        bool f=0;
-        
-        for(int i=0;i<s.size()-1;i++){
-            if(s[i] == 'I' and s[i+1]=='V') {ans+=4; i++;
-                                             if(i==s.size()-1) {;f=1;}}
-            else if(s[i] == 'I' and s[i+1]=='X') {ans+=9; i++;
-                                                   if(i==s.size()-1){f=1;}}
-            else if(s[i] == 'X' and s[i+1]=='L') {ans+=40; i++;
-                                                   if(i==s.size()-1) {f=1;}}
-             else if(s[i] == 'X' and s[i+1]=='C') {ans+=90; i++;
-                                                    if(i==s.size()-1){f=1;}}
-             else if(s[i] == 'C' and s[i+1]=='D') {ans+=400; i++;
-                                                    if(i==s.size()-1) {f=1;}}
-             else if(s[i] == 'C' and s[i+1]=='M') {ans+=900; i++;
-                                                    if(i==s.size()-1) {f=1;}}
-            
-            else if(s[i]=='I') ans+=1;
-        else if(s[i]=='V') {ans+=5;}
-        else if(s[i]=='X') ans+=10;
-    else if(s[i]=='L') ans+=50;
-            else if(s[i]=='C') ans+=100;
-            else if(s[i]=='D') ans+=500;
-   else if(s[i]=='M') ans+=1000;
-        } 
-        
-       if(!f){
-           int i=s.size()-1;
-          // cout<<"yr";
-             if(s[i]=='I') ans+=1;
-        else if(s[i]=='V') ans+=5;
-        else if(s[i]=='X') ans+=10;
-    else if(s[i]=='L') ans+=50;
-            else if(s[i]=='C') ans+=100;
-            else if(s[i]=='D') ans+=500;
-   else if(s[i]=='M') ans+=1000;
-       }
-        return ans;
+int romanToInt(string s) {
+unordered_map<char,int> mp{{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+    int ans =0;
+    for(int i=0;i<s.size();i++){
+        if(mp[s[i]]<mp[s[i+1]]) ans-=mp[s[i]];
+        else ans+=mp[s[i]];
     }
+    return ans;
+}
 };
