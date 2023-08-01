1440535711d55a4a7982993584bccbc5eaf9ec28
Shubham Yadav <shubh08am@gmail.com>
2023-08-01 22:22:15 +0530

    Create 117. Populating Next Right Pointers in Each Node II.cpp

---
+++ 117. Populating Next Right Pointers in Each Node II.cpp
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        //only diff is that it is not a perfect binary tree 
+        if(!root) return NULL;
+        queue<Node*>q;
+        q.push(root) ; 
+        q.push(NULL) ; 
+        while(!q.empty()){
+            int sz = q.size() ; 
+            while(sz--){
+                Node*curr = q.front() ; 
+                q.pop() ; 
+                if(!curr){
+                   if(!q.empty()) q.push(NULL);
+                }
+                else{
+                    curr->next = q.front() ; 
+                    if(curr->left) q.push(curr->left) ; 
+                    if(curr->right) q.push(curr->right) ; 
+                }
+            }
+        }
+    return root;
+    }
+};
