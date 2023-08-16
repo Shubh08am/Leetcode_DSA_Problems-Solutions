bfa387c33af87d6aa2e5455ee5a1d0d4527976d2
Shubham Yadav <shubh08am@gmail.com>
2023-08-16 16:38:49 +0530

    Update 38. Count and Say.cpp

--- 38. Count and Say.cpp
+++ 38. Count and Say.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    string countAndSay(int n) {
-  
-  if(n == 1){
-            return "1";
-        }
-        
-        string s = countAndSay(n - 1);
-        string res = "";
-        for(int i = 0; i<s.size(); i++){
-            int cnt = 1;
-            while(i < s.size() - 1 and s[i] == s[i + 1]){
-                i++;
-                cnt++;
-            }
-            string ch = to_string(cnt) + s[i] ;
-            res+=ch;
-        }
-        return res;
+    string solve(int n){
+        if(n==1) return "1" ; 
 
+        string curr = solve(n-1) ; 
+        //cout<<n<<" "<<curr<<"\n";
+        string res = "" ; 
+        for(int i=0;i<curr.size();i++){
+            char ch = curr[i] ; 
+            int occur = 1 ; 
+            //count occurence -> like one two three .. 
+            while(i+1<curr.size()&&curr[i]==curr[i+1]){
+                i++;
+                occur++;
+            }
+            //change to string now -> ex:- 1 -> one 1 -> 1 1
+            res+=to_string(occur)+ch;
+            //cout<<n<<" "<<res<<"\n";
+        }
+    return res;
+    }
+    string countAndSay(int n) {
+        return solve(n) ; 
     }
 };
