f7cb597d3b3adaf01a7eb3d43152c9e1c55b2bc2
Shubham Yadav <shubh08am@gmail.com>
2023-08-22 19:22:32 +0530

    Update 238. Product of Array Except Self.cpp

--- 238. Product of Array Except Self.cpp
+++ 238. Product of Array Except Self.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
+    vector<int> productExceptSelf2(vector<int>& nums) {
+        // prefix - suffix 
+        int n  = nums.size() ; 
+        vector<int>preffix(n) , suffix(n) , ans(n); 
+        preffix[0] = suffix[n-1] = 1 ; 
+        //product of every element before i
+        for(int i=1;i<n;i++){
+            preffix[i] = preffix[i-1] * nums[i-1] ; 
+        } 
+        //product of every element after i
+        for(int i=n-2;i>=0;i--){
+            suffix[i] = suffix[i+1]*nums[i+1] ;
+        }
+        //preffix[i] * suffix[i] contains product of every element before i and every element after i but not the element at index i (and that is the reson why we excluded a[i] in our prefix and suffix product).
+        for(int i=0;i<n;i++){
+            ans[i] = preffix[i] * suffix[i] ; 
+        } 
+    return ans;
+    }
     vector<int> productExceptSelf(vector<int>& nums) {
-   int n = nums.size();
-        vector<int> res,l(n),r(n);    
-        l[0] = 1; r[n - 1] = 1;
-
-        for(int i = 1; i < n; i++){
-            l[i] = l[i - 1] * nums[i - 1];
+        // no need of separate prefix - suffix directly store it in answer 
+        //O(1) SC
+        int n  = nums.size() ; 
+        vector<int> ans(n,1); 
+        int product = 1 ; 
+        //prffix part
+        for(int i=0;i<n;i++){
+            ans[i] = ans[i]*product ; 
+            product*=nums[i];
         }
-
-        for (int j = n - 2; j >= 0; j--) {
-            r[j] = r[j + 1] * nums[j + 1];
+        //suffix part
+        product=1;
+        for(int i=n-1;i>=0;i--){
+            ans[i] = ans[i]*product;
+            product*=nums[i];
         }
-        
-        for (int k = 0; k < n; k++) {
-            int value = l[k] * r[k];
-            res.push_back(value);
-        }
-        
-        return res;       
+    return ans;
     }
 };
