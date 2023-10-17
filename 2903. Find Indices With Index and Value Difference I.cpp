42ec7a8862c48ff6c80583a414e8d5952e53eda2
Shubham Yadav <shubh08am@gmail.com>
2023-10-18 00:19:21 +0530

    Create 2903. Find Indices With Index and Value Difference I.cpp

---
+++ 2903. Find Indices With Index and Value Difference I.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> findIndices(vector<int>& nums, int indexDifference, int valueDifference) {
+        int n = nums.size() ; 
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(abs(i-j)>=indexDifference && abs(nums[i] - nums[j]) >= valueDifference) return {i,j};
+            }
+        }
+        return {-1,-1};
+    }
+};
