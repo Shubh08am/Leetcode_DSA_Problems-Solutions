ac3d5101eef25d5df5ff22a650e213f9e622b485
Shubham Yadav <shubh08am@gmail.com>
2024-01-03 16:13:50 +0530

    Update 1140. Stone Game II.cpp

--- 1140. Stone Game II.cpp
+++ 1140. Stone Game II.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
-    int solve(int ind,int m,vector<int> &piles,vector<vector<int>> &dp){
-        int n = piles.size();
+    int solve(int i,int m,bool turn,vector<int>& piles,vector<vector<vector<int>>>&dp) {
+        int n=piles.size();
+        if(i>=n) return 0;
+        int &val=dp[i][m][turn];
+        if(val!=-1) return val;
 
-        if(ind >= piles.size()) return 0;
+        //if turn=1 Alice goes and maximize the result therefore take result as INT_MIN 
+        //if turn=0 Bob goes and minimize the result therefore take result as INT_MAX
+        int result = (turn==1?INT_MIN:INT_MAX),stones=0;
 
-        if(dp[ind][m]!=-1) return dp[ind][m];
-
-        int ans = -1e9;
-        int score = 0;
-
-        for(int i = ind;i<(ind + 2*m) && i<n ;i++){
-            score += piles[i];
-            ans = max(ans,score - solve(i+1,max((i+1 - ind),m),piles,dp));
+        //Go till min of n-i or 2*m 
+        for(int x=1;x<=min(n-i,2*m);x++){
+            stones+=piles[i+x-1]; //this act as prefix sum 
+            //Alice goes -> Next time than bob goes(turn 0) -> update m by max(m,x) here x represents last times no of piles choosen -> update i to i+x as we took x piles 
+            if(turn==1){
+                result=max(result,stones+solve(i+x,max(m,x),0,piles,dp));
+            }
+            //Bob goes -> Next time than Alice goes(turn 1) -> update m by max(m,x) here x represents last times no of piles choosen -> update i to i+x as we took x piles 
+            else{
+                result=min(result,solve(i+x,max(m,x),1,piles,dp));
+            }
         }
-
-        return dp[ind][m] = ans;
+    return val=result;
     }
     int stoneGameII(vector<int>& piles) {
-        int n = piles.size();
-        int sum = accumulate(piles.begin(),piles.end(),0);
-        vector<vector<int>> dp(n+1,vector<int> (n+1,-1));
-        int ans = (sum + solve(0,1,piles,dp))/2;
-
-        return ans;
+        //[1,2,3,4,5,100]
+        //1 alice  
+        //2 bob 
+        //3 alice 
+        //4,5 bob 
+        //100 alice -> Thus, total alice = 104 
+    int n=piles.size();
+    vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(n+1,vector<int>(2,-1)));
+    //1 indicates Alice and 2 indicates bob
+    return solve(0,1,1,piles,dp);
     }
 };
